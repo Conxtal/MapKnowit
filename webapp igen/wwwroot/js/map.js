42a8e9528ce233691bedd77f1e8e4ed40d6a62ca
Nicklas Nystedt <niny0628@storsjogymnasiet.se>
2023-10-09 14:45:55 +0200

    map

---
+++ webapp igen/wwwroot/js/map.js
@@ -0,0 +1,31 @@
+fetch("Data/sitecluster.json")
+    .then(response => response.json())
+    .then(data => {
+        var map = L.map('map').setView([59.3789291381836, 16.4947643280029], 7);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+            maxZoom: 18,
+            minZoom: 3
+        }).addTo(map);
+
+        var filtredSites = data.Locations; 
+
+        var markers = L.markerClusterGroup();
+
+        for (var i = 0; i < filtredSites.length; i++) { 
+            var site = filtredSites[i]; 
+
+            var myIcon = L.icon({ 
+                iconUrl: 'imgs/AA-marker-icon.png',
+                iconSize: [18, 18],
+                shadowSize: [41, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [0, -41]
+            });
+            var marker = L.marker([site.lat, site.lon], { icon: myIcon }).addTo(map);
+            markers.addLayer(marker);
+        }
+
+        map.addLayer(markers); // Add the markers layer to the map
+    });
